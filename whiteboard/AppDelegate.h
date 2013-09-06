9a3c1555a4a4f4f5eb32c5d0ea2eafc1f0269ef1
sad-fueled <sad@fueled.com>
2013-09-06 17:12:10 -0400

    Creates Xcode project with gitignore

---
+++ whiteboard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  whiteboard
+//
+//  Created by Sacha Durand Saint Omer on 9/6/13.
+//  Copyright (c) 2013 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ whiteboard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  whiteboard
+//
+//  Created by Sacha Durand Saint Omer on 9/6/13.
+//  Copyright (c) 2013 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
