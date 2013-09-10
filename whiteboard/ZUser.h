350919b2402db5ce1ff89de4e4933db1e6fe6911
lnf-fueled <lnf@fueled.com>
2013-09-10 16:30:15 -0400

    Added a class comment to ZUser

--- whiteboard/ZUser.h
+++ whiteboard/ZUser.h
@@ -9,6 +9,9 @@
 #import <Foundation/Foundation.h>
 #import "WBUser.h"
 
+/**
+ This is a sample concerete implementation of WBUser that was used for writing unit tests.
+ */
 @interface ZUser : NSObject <WBUser>
 
 @end
