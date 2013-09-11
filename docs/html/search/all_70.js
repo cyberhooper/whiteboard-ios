297df861f392aa63bc5b70044f8b0535be0d7667
lnf-fueled <lnf@fueled.com>
2013-09-11 11:18:14 -0400

    Modified main page and regenerated docs.

--- docs/html/search/all_70.js
+++ docs/html/search/all_70.js
@@ -3,7 +3,6 @@ var searchData=
   ['parsedatasource',['ParseDataSource',['../interface_parse_data_source.html',1,'']]],
   ['parsedatasource_2eh',['ParseDataSource.h',['../_parse_data_source_8h.html',1,'']]],
   ['parsedatasource_2em',['ParseDataSource.m',['../_parse_data_source_8m.html',1,'']]],
-  ['parseprofilepicturemediumurl',['parseProfilePictureMediumURL',['../interface_parse_user.html#a976d288343bf867a8786ebc9bbba01bd',1,'ParseUser']]],
   ['parseuser',['ParseUser',['../interface_parse_user.html',1,'']]],
   ['parseuser_2eh',['ParseUser.h',['../_parse_user_8h.html',1,'']]],
   ['parseuser_2em',['ParseUser.m',['../_parse_user_8m.html',1,'']]],
--- docs/html/search/files_6d.js
+++ docs/html/search/files_6d.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['main_2em',['main.m',['../main_8m.html',1,'']]],
+  ['mainpage_2edox',['mainpage.dox',['../mainpage_8dox.html',1,'']]],
   ['myviewcontroller_2eh',['MyViewController.h',['../_my_view_controller_8h.html',1,'']]],
   ['myviewcontroller_2em',['MyViewController.m',['../_my_view_controller_8m.html',1,'']]]
 ];
