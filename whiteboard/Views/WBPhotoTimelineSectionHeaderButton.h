90ecbc864bb90cb5bcfedc3ef310a322b3e7b43b
prs-fueled <prs@fueled.com>
2013-09-16 11:28:51 -0400

    Initial section header button commit

---
+++ whiteboard/Views/WBPhotoTimelineSectionHeaderButton.h
@@ -0,0 +1,13 @@
+//
+//  WBPhotoTimelineSectionHeaderButton.h
+//  whiteboard
+//
+//  Created by prs-fueled on 9/13/13.
+//  Copyright (c) 2013 Fueled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WBPhotoTimelineSectionHeaderButton : UIView
+
+@end
---
+++ whiteboard/Views/WBPhotoTimelineSectionHeaderButton.m
@@ -0,0 +1,27 @@
+//
+//  WBPhotoTimelineSectionHeaderButton.m
+//  whiteboard
+//
+//  Created by prs-fueled on 9/13/13.
+//  Copyright (c) 2013 Fueled. All rights reserved.
+//
+
+#import "WBPhotoTimelineSectionHeaderButton.h"
+
+@implementation WBPhotoTimelineSectionHeaderButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+  self = [super initWithFrame:frame];
+  if (self) {
+    [self setupView];
+  }
+  return self;
+}
+
+#pragma mark - Setup
+- (void)setupView {
+  
+}
+
+@end
