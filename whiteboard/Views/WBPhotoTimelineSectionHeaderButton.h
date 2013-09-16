928a3a21b06989869b1d61202207a30d71b63b78
prs-fueled <prs@fueled.com>
2013-09-16 15:10:44 -0400

    Added section header button

--- whiteboard/Views/WBPhotoTimelineSectionHeaderButton.h
+++ whiteboard/Views/WBPhotoTimelineSectionHeaderButton.h
@@ -8,6 +8,37 @@
 
 #import <UIKit/UIKit.h>
 
+@class WBPhotoTimelineSectionHeaderButton;
+
+@protocol WBPhotoTimelineSectionHeaderButtonDelegate <NSObject>
+- (void)wbPhotoTimelineSectionHeaderButtonPressed:(WBPhotoTimelineSectionHeaderButton *)button;
+@end
+
 @interface WBPhotoTimelineSectionHeaderButton : UIView
+
+/**
+ The image to be displayed in the view in normal state
+ */
+@property (nonatomic, strong) UIImage *normalImage;
+
+/**
+ The image to be displayed in the view in highlighted state
+ */
+@property (nonatomic, strong) UIImage *highlightedImage;
+
+/**
+ The image to be displayed in the view in selected state
+ */
+@property (nonatomic, strong) UIImage *selectedImage;
+
+/**
+ The number label to be displayed inside the button
+ */
+@property (nonatomic, strong) UILabel *numberLabel;
+
+/**
+ The delegate for this class
+ */
+@property (nonatomic, weak) id<WBPhotoTimelineSectionHeaderButtonDelegate> delegate;
 
 @end
