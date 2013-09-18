e7e2646c0889ec12c78e7c8cc801cded458fe55f
lnf-fueled <lnf@fueled.com>
2013-09-18 11:45:46 -0400

    Added empty setup method to the header view.

--- whiteboard/Views/WBProfileHeaderView.m
+++ whiteboard/Views/WBProfileHeaderView.m
@@ -15,17 +15,21 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+      [self setUpView];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+- (void)awakeFromNib {
+  [super awakeFromNib];
+  
+  [self setUpView];
 }
-*/
+
+#pragma mark - Setup
+
+- (void)setUpView {
+  
+}
 
 @end
